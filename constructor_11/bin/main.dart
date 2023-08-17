edde1689df962274b775bd3688985c855a908c99
bhaskarayh <bhaskarayh@gmail.com>
2023-08-17 20:38:13 +0700

    first commit belajar PBO dart v2

---
+++ constructor_11/bin/main.dart
@@ -0,0 +1,8 @@
+import 'person.dart';
+
+void main(List<String> arguments) {
+  Person p = Person();
+  p.name = "Joko";
+
+  print(p.name);
+}
---
+++ constructor_11/test/constructor_11_test.dart
@@ -0,0 +1,8 @@
+import 'package:constructor_11/constructor_11.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
